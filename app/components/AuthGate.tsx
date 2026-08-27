bc3db5aa9d7b7f1c0d08a0888424d28d719b6c2c
Hritik Raj <hritik983567@gmail.com>
2026-08-27 21:30:36 +0530

    feat: 2.8s multi-second F1 car entry scene with speed trail streaks

--- app/components/AuthGate.tsx
+++ app/components/AuthGate.tsx
@@ -172,6 +172,12 @@ export default function AuthGate() {
       overflowY: 'auto'
     }}>
       <div className="login-wallpaper-wrapper">
+        <div className="f1-entry-speed-streaks">
+          <span className="streak streak-1"></span>
+          <span className="streak streak-2"></span>
+          <span className="streak streak-3"></span>
+          <span className="streak streak-4"></span>
+        </div>
         <img 
           src="/images/f1-login-car.png" 
           alt="Formula 1 Car Wallpaper" 
